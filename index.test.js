1a343d5fecec02d6c18977d775e5fac7d3a94231
David Lam <davidlam.yc@gmail.com>
2020-05-20 10:50:31 +0800

    test

---
+++ index.test.js
@@ -0,0 +1,17 @@
+const calculator = require('./index');
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(calculator.add(1, 2)).toBe(3);
+});
+
+test('minus 2 - 1 to equal 1', () => {
+    expect(calculator.minus(2, 1)).toBe(1);
+});
+
+test('multiply 2 x 2 to equal 4', () => {
+    expect(calculator.multiply(2, 2)).toBe(4);
+});
+
+test('divide 6 x 2 to equal 3', () => {
+    expect(calculator.divide(6, 2)).toBe(3);
+});
